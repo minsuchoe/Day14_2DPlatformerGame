064d102f5c039c43ef134684957276d0a9fc5c30
minsu choe <choi.minsoo.msc@gmail.com>
2018-02-26 18:06:34 +0900

    Day14_2DPlatformerGame

---
+++ Assets/SpawnCoins.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCoins : MonoBehaviour {
+    public Transform[] coinSpawns;
+    public GameObject coin;
+
+	// Use this for initialization
+	void Start () {
+        Spawn();
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+		for (int i = 0; i < coinSpawns.Length; i++)
+        {
+            int coinFlip = Random.Range(0, 2);  // (int타입) 0부터 '1'까지
+            if (coinFlip > 0)
+                Instantiate(coin, coinSpawns[i].position, Quaternion.identity);
+        }
+	}
+}
